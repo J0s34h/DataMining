3b6446cef1d665ab24fe37043997de96dc07415c
Joseph <yusuf.fayzullin@gmail.com>
2021-02-17 10:36:32 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_helloworld():
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hello World")  # Press ⌘F8 to toggle the breakpoint.
+
+print_helloworld()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
