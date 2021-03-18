e71dd289bf5de041679c7f246192cfcf0e3776f7
Joseph <yusuf.fayzullin@gmail.com>
2021-03-18 15:45:37 +0300

    Dag Script V4.5

--- dag_script.py
+++ dag_script.py
@@ -1,6 +1,5 @@
 import datetime
 import re
-from datetime import timedelta
 import emoji
 import nltk
 import psycopg2
@@ -8,7 +7,6 @@ import vk_api
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 from nltk.corpus import stopwords
-from psycopg2._psycopg import cursor
 
 args = {
     'owner': 'airflow',
@@ -17,6 +15,17 @@ args = {
     'retry_delay': datetime.timedelta(minutes=1),
     'depends_on_past': False,
 }
+
+nltk.download('stopwords')
+
+# Local Variables
+TOKEN = "fa267307fa267307fa2673074ffa50e889ffa26fa2673079a11a84f2cb2d50acb0df095"
+url_filter = r'\S((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*\S'
+meta_char_filter = r'[!@#»«$%^&*(;":,./<>?`~=_+\]\}\{\[\|\-)r]'
+club_filter = r'club[0-9]+[A-Z]*\S*'
+post_cache = {}
+
+conn = psycopg2
 
 nltk.download('stopwords')
 
@@ -47,8 +56,11 @@ def split(word):
 
 
 def word_handler(word):
+    if word in stopwords.words('russian'):
+        return
+
     word = word.replace("\n", "")
-    if not (re.search(url_filter, repr(word)) or word in stopwords.words('russian')):
+    if not (re.search(url_filter, repr(word))):
         word = re.sub(club_filter, "", word)
         word = re.sub(meta_char_filter, "", word)
         if word != "":
@@ -59,14 +71,14 @@ def word_handler(word):
                 post_cache[raw_word] = 0
 
 
-def post_handler(post):
+def post_handler(post, cursor):
     for key in post:
-        cursor.execute('select * from COUNTER where word=%s', (key,))
+        cursor.execute('SELECT * FROM COUNTER WHERE word=%s', (key,))
         if cursor.rowcount != 0:
             row = cursor.fetchone()
             cursor.execute('UPDATE COUNTER SET ENCOUNTERS = %s  WHERE word=%s', (row[1] + 1, key))
         else:
-            cursor.execute('INSERT INTO COUNTER (WORD, ENCOUNTERS) VALUES (%s, %s)', (key, 0))
+            cursor.execute('INSERT INTO COUNTER (WORD, ENCOUNTERS) VALUES (%s, %s)', (key, 1))
 
 
 def main():
@@ -74,14 +86,13 @@ def main():
         print("Connecting to database")
         conn = psycopg2.connect(dbname='', user='postgres',
                                 password='Erika1944', host='database-1.cfrqfkiv7xzd.us-east-1.rds.amazonaws.com')
-        cursor = conn.cursor()
-        cursor.execute("DROP TABLE IF EXISTS COUNTER")
-
+        curs = conn.cursor()
+        curs.execute("DROP TABLE IF EXISTS COUNTER")
         sql = '''CREATE TABLE COUNTER(
-           WORD VARCHAR NOT NULL UNIQUE,
-           ENCOUNTERS INT NOT NULL
+        WORD VARCHAR NOT NULL UNIQUE,
+        ENCOUNTERS INT NOT NULL
         )'''
-        cursor.execute(sql)
+        curs.execute(sql)
     except psycopg2.errors as e:
         print('Error %s' % e)
         return
@@ -101,7 +112,7 @@ def main():
         return
 
     tools = vk_api.VkTools(vk_session)
-    wall = tools.get_all_slow_iter('wall.get', 1, {'domain': "itis_kfu"})
+    wall = tools.get_all_iter('wall.get', 1, {'domain': "itis_kfu"})
 
     print("Starting processing posts")
 
@@ -117,7 +128,7 @@ def main():
                 word = ""
             elif char_checker(char):
                 word += char
-        post_handler(post_cache)
+        post_handler(post_cache, curs)
         post_cache.clear()
         conn.commit()
         count += 1
@@ -127,7 +138,7 @@ def main():
     conn.close()
 
 
-dag = DAG('dag_script', schedule_interval='daily',
+dag = DAG('dag_script', schedule_interval=None,
           default_args=args)
 
 t1 = PythonOperator(
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import vk_api
 # DB
 import psycopg2
 import psycopg2.errors
-from psycopg2._psycopg import cursor
 # Filter
 import emoji
 import re
@@ -41,8 +40,11 @@ def split(word):
 
 
 def word_handler(word):
+    if word in stopwords.words('russian'):
+        return
+
     word = word.replace("\n", "")
-    if not (re.search(url_filter, repr(word)) or word in stopwords.words('russian')):
+    if not (re.search(url_filter, repr(word))):
         word = re.sub(club_filter, "", word)
         word = re.sub(meta_char_filter, "", word)
         if word != "":
@@ -53,14 +55,14 @@ def word_handler(word):
                 post_cache[raw_word] = 0
 
 
-def post_handler(post):
+def post_handler(post, cursor):
     for key in post:
-        cursor.execute('select * from COUNTER where word=%s', (key,))
+        cursor.execute('SELECT * FROM COUNTER WHERE word=%s', (key,))
         if cursor.rowcount != 0:
             row = cursor.fetchone()
             cursor.execute('UPDATE COUNTER SET ENCOUNTERS = %s  WHERE word=%s', (row[1] + 1, key))
         else:
-            cursor.execute('INSERT INTO COUNTER (WORD, ENCOUNTERS) VALUES (%s, %s)', (key, 0))
+            cursor.execute('INSERT INTO COUNTER (WORD, ENCOUNTERS) VALUES (%s, %s)', (key, 1))
 
 
 def main():
@@ -68,14 +70,13 @@ def main():
         print("Connecting to database")
         conn = psycopg2.connect(dbname='', user='postgres',
                                 password='Erika1944', host='database-1.cfrqfkiv7xzd.us-east-1.rds.amazonaws.com')
-        cursor = conn.cursor()
-        cursor.execute("DROP TABLE IF EXISTS COUNTER")
-
+        curs = conn.cursor()
+        curs.execute("DROP TABLE IF EXISTS COUNTER")
         sql = '''CREATE TABLE COUNTER(
-           WORD VARCHAR NOT NULL UNIQUE,
-           ENCOUNTERS INT NOT NULL
+        WORD VARCHAR NOT NULL UNIQUE,
+        ENCOUNTERS INT NOT NULL
         )'''
-        cursor.execute(sql)
+        curs.execute(sql)
     except psycopg2.errors as e:
         print('Error %s' % e)
         return
@@ -111,7 +112,7 @@ def main():
                 word = ""
             elif char_checker(char):
                 word += char
-        post_handler(post_cache)
+        post_handler(post_cache, curs)
         post_cache.clear()
         conn.commit()
         count += 1
