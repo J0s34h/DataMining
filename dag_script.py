292d619391849cbadff7cfe550695949bca4f0e4
Joseph <yusuf.fayzullin@gmail.com>
2021-03-18 17:16:00 +0300

    Dag Script V4.7 Converted all into one function call

--- dag_script.py
+++ dag_script.py
@@ -12,74 +12,64 @@ args = {
     'owner': 'airflow',
     'start_date': datetime.datetime(2021, 3, 18),
     'retries': 1,
-    'retry_delay': datetime.timedelta(minutes=1),
+    'retry_delay': datetime.timedelta(minutes=20),
     'depends_on_past': False,
 }
 
-# Local Variables
-TOKEN = "fa267307fa267307fa2673074ffa50e889ffa26fa2673079a11a84f2cb2d50acb0df095"
-url_filter = r'\S((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*\S'
-meta_char_filter = r'[!@#»«$%^&*(;":,./<>?`~=_+\]\}\{\[\|\-)r]'
-club_filter = r'club[0-9]+[A-Z]*\S*'
-post_cache = {}
 
-conn = psycopg2
+def logic_function():
+    # Local Variables
+    TOKEN = "fa267307fa267307fa2673074ffa50e889ffa26fa2673079a11a84f2cb2d50acb0df095"
+    url_filter = r'\S((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*\S'
+    meta_char_filter = r'[!@#»«$%^&*(;":,./<>?`~=_+\]\}\{\[\|\-)r]'
+    club_filter = r'club[0-9]+[A-Z]*\S*'
+    post_cache = {}
 
-def captcha_handler(captcha):
-    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
-    return captcha.try_again(key)
+    conn = psycopg2
 
+    def char_checker(char) -> bool:
+        if char in emoji.UNICODE_EMOJI_ENGLISH:
+            return False
+        else:
+            return True
 
-def char_checker(char) -> bool:
-    if char in emoji.UNICODE_EMOJI_ENGLISH:
-        return False
-    else:
-        return True
+    def split(word):
+        return [char for char in word]
 
+    def word_handler(word):
+        word = word.replace("\n", "")
+        if not (re.search(url_filter, repr(word))):
+            word = re.sub(club_filter, "", word)
+            word = re.sub(meta_char_filter, "", word)
+            if word != "":
+                raw_word = word.lower()
+                if post_cache.__contains__(raw_word):
+                    post_cache[raw_word] += 1
+                else:
+                    post_cache[raw_word] = 0
 
-def split(word):
-    return [char for char in word]
-
-
-def word_handler(word):
-    word = word.replace("\n", "")
-    if not (re.search(url_filter, repr(word))):
-        word = re.sub(club_filter, "", word)
-        word = re.sub(meta_char_filter, "", word)
-        if word != "":
-            raw_word = word.lower()
-            if post_cache.__contains__(raw_word):
-                post_cache[raw_word] += 1
-            else:
-                post_cache[raw_word] = 0
-
-
-def post_handler(post, cursor):
-    for key in post:
-        cursor.execute('SELECT * FROM COUNTER WHERE word=%s', (key,))
+    def post_handler(post):
+        for key in post:
+            cursor.execute('SELECT * FROM COUNTER WHERE word=%s', (key,))
         if cursor.rowcount != 0:
             row = cursor.fetchone()
             cursor.execute('UPDATE COUNTER SET ENCOUNTERS = %s  WHERE word=%s', (row[1] + 1, key))
         else:
             cursor.execute('INSERT INTO COUNTER (WORD, ENCOUNTERS) VALUES (%s, %s)', (key, 1))
 
-
-def main():
     try:
         print("Connecting to database")
         conn = psycopg2.connect(dbname='', user='postgres',
                                 password='Erika1944', host='database-1.cfrqfkiv7xzd.us-east-1.rds.amazonaws.com')
-        curs = conn.cursor()
-        curs.execute("DROP TABLE IF EXISTS COUNTER")
+        cursor = conn.cursor()
+        cursor.execute('DROP TABLE IF EXISTS COUNTER')
         sql = '''CREATE TABLE COUNTER(
-        WORD VARCHAR NOT NULL UNIQUE,
-        ENCOUNTERS INT NOT NULL
-        )'''
-        curs.execute(sql)
+                    WORD VARCHAR NOT NULL UNIQUE,
+                    ENCOUNTERS INT NOT NULL
+                    )'''
+        cursor.execute(sql)
     except psycopg2.errors as e:
         print('Error %s' % e)
-        return
-
     print("Successful connection to database")
 
     vk_session = vk_api.VkApi(
@@ -92,7 +82,6 @@ def main():
         vk_session.get_api()
     except vk_api.AuthError as error_msg:
         print(error_msg)
-        return
 
     tools = vk_api.VkTools(vk_session)
     wall = tools.get_all_iter('wall.get', 1, {'domain': "itis_kfu"})
@@ -111,13 +100,11 @@ def main():
                 word = ""
             elif char_checker(char):
                 word += char
-        post_handler(post_cache, curs)
+        post_handler(post_cache)
         post_cache.clear()
         conn.commit()
         count += 1
-
     print("Total " + str(count) + " posts processed")
-
     conn.close()
 
 
@@ -127,4 +114,4 @@ dag = DAG('dag_script', schedule_interval=None,
 t1 = PythonOperator(
     task_id='dag_script',
     dag=dag,
-    python_callable=main)
+    python_callable=logic_function)
--- main.py
+++ main.py
@@ -9,71 +9,60 @@ import psycopg2.errors
 import emoji
 import re
 
-# Local Variables
-TOKEN = "fa267307fa267307fa2673074ffa50e889ffa26fa2673079a11a84f2cb2d50acb0df095"
-url_filter = r'\S((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*\S'
-meta_char_filter = r'[!@#»«$%^&*(;":,./<>?`~=_+\]\}\{\[\|\-)r]'
-club_filter = r'club[0-9]+[A-Z]*\S*'
-post_cache = {}
 
-conn = psycopg2
+def logic_function():
+    # Local Variables
+    TOKEN = "fa267307fa267307fa2673074ffa50e889ffa26fa2673079a11a84f2cb2d50acb0df095"
+    url_filter = r'\S((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*\S'
+    meta_char_filter = r'[!@#»«$%^&*(;":,./<>?`~=_+\]\}\{\[\|\-)r]'
+    club_filter = r'club[0-9]+[A-Z]*\S*'
+    post_cache = {}
 
+    conn = psycopg2
 
-def captcha_handler(captcha):
-    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
-    return captcha.try_again(key)
+    def char_checker(char) -> bool:
+        if char in emoji.UNICODE_EMOJI_ENGLISH:
+            return False
+        else:
+            return True
 
+    def split(word):
+        return [char for char in word]
 
-def char_checker(char) -> bool:
-    if char in emoji.UNICODE_EMOJI_ENGLISH:
-        return False
-    else:
-        return True
+    def word_handler(word):
+        word = word.replace("\n", "")
+        if not (re.search(url_filter, repr(word))):
+            word = re.sub(club_filter, "", word)
+            word = re.sub(meta_char_filter, "", word)
+            if word != "":
+                raw_word = word.lower()
+                if post_cache.__contains__(raw_word):
+                    post_cache[raw_word] += 1
+                else:
+                    post_cache[raw_word] = 0
 
-
-def split(word):
-    return [char for char in word]
-
-
-def word_handler(word):
-    word = word.replace("\n", "")
-    if not (re.search(url_filter, repr(word))):
-        word = re.sub(club_filter, "", word)
-        word = re.sub(meta_char_filter, "", word)
-        if word != "":
-            raw_word = word.lower()
-            if post_cache.__contains__(raw_word):
-                post_cache[raw_word] += 1
-            else:
-                post_cache[raw_word] = 0
-
-
-def post_handler(post, cursor):
-    for key in post:
-        cursor.execute('SELECT * FROM COUNTER WHERE word=%s', (key,))
+    def post_handler(post):
+        for key in post:
+            cursor.execute('SELECT * FROM COUNTER WHERE word=%s', (key,))
         if cursor.rowcount != 0:
             row = cursor.fetchone()
             cursor.execute('UPDATE COUNTER SET ENCOUNTERS = %s  WHERE word=%s', (row[1] + 1, key))
         else:
             cursor.execute('INSERT INTO COUNTER (WORD, ENCOUNTERS) VALUES (%s, %s)', (key, 1))
 
-
-def main():
     try:
         print("Connecting to database")
         conn = psycopg2.connect(dbname='', user='postgres',
                                 password='Erika1944', host='database-1.cfrqfkiv7xzd.us-east-1.rds.amazonaws.com')
-        curs = conn.cursor()
-        curs.execute("DROP TABLE IF EXISTS COUNTER")
+        cursor = conn.cursor()
+        cursor.execute('DROP TABLE IF EXISTS COUNTER')
         sql = '''CREATE TABLE COUNTER(
-        WORD VARCHAR NOT NULL UNIQUE,
-        ENCOUNTERS INT NOT NULL
-        )'''
-        curs.execute(sql)
+                    WORD VARCHAR NOT NULL UNIQUE,
+                    ENCOUNTERS INT NOT NULL
+                    )'''
+        cursor.execute(sql)
     except psycopg2.errors as e:
         print('Error %s' % e)
-        return
-
     print("Successful connection to database")
 
     vk_session = vk_api.VkApi(
@@ -86,7 +75,6 @@ def main():
         vk_session.get_api()
     except vk_api.AuthError as error_msg:
         print(error_msg)
-        return
 
     tools = vk_api.VkTools(vk_session)
     wall = tools.get_all_slow_iter('wall.get', 1, {'domain': "itis_kfu"})
@@ -105,15 +93,12 @@ def main():
                 word = ""
             elif char_checker(char):
                 word += char
-        post_handler(post_cache, curs)
+        post_handler(post_cache)
         post_cache.clear()
         conn.commit()
         count += 1
-
     print("Total " + str(count) + " posts processed")
-
     conn.close()
 
 
-if __name__ == '__main__':
-    main()
+logic_function()
